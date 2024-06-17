a9fba764d7b1bac266306957efe9fd26b4d21884
vasudev kumawat <130001057+vasudev200@users.noreply.github.com>
2024-06-17 08:12:37 +0530

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,6 @@
+// const hamburger = document.querySelector('.togle');
+//         const navLinks = document.querySelector('.nav-links');
+
+//         hamburger.addEventListener('click', () => {
+//             navLinks.classList.toggle('open');
+//         });
