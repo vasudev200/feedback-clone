0023c93d7d1d494bc65dee8a652a904711badaa1
vasudev kumawat <130001057+vasudev200@users.noreply.github.com>
2024-06-17 08:10:31 +0530

    Delete feedback-clone directory

--- feedback-clone/assets/js/theme.js
+++
@@ -1,6 +0,0 @@
-// const hamburger = document.querySelector('.togle');
-//         const navLinks = document.querySelector('.nav-links');
-
-//         hamburger.addEventListener('click', () => {
-//             navLinks.classList.toggle('open');
-//         });
